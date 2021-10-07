932a95dd8f73772fb98354822ec9a9edc1621e15
Jacek Stasiak <snsvgb@wp.pl>
2021-10-07 13:30:17 +0200

    Reversing text

--- zad1/zad1.py
+++ zad1/zad1.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def empty_string():
+	return ""
+
+@app.route("/<string:input_text>")
+def reverse_string(input_text):
+    return input_text[::-1]
