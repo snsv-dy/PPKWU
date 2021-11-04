c9af45fe4685c601b3d12284e81c23274f8a6b75
Jacek Stasiak <snsvgb@wp.pl>
2021-11-04 14:05:08 +0100

    Remaining formats

--- zad3/app.py
+++ zad3/app.py
@@ -8,8 +8,11 @@ app = Flask(__name__)
 print(f'Remote api url: {os.environ["API_URL"]}')
 
 content_types = {
-	"txt": "text/plain",
-	"json": "application/json"
+	"txt": "text/plain; charset=UTF-8",
+	"json": "application/json",
+	"xml": "text/xml",
+	"csv": "text/csv; charset=UTF-8",
+	None: "text/plain; charset=UTF-8"
 }
 
 def format_result(format, value):
@@ -18,11 +21,19 @@ def format_result(format, value):
 		result = f"Rodzaj znaków: {value}."
 	elif format == 'json':
 		result = "{ \"rodzaj_znakow\": " + value + " }"
+	elif format == 'xml':
+		result = f"""<?xml version="1.0" encoding="UTF-8"?>
+<rodzaj_znakow>{value}</rodzaj_znakow>
+		"""
+	elif format == 'csv':
+		result = f"""\"Rodzaj znaków\"
+{value}
+		"""
 	else:
-		raise Exception()
+		result = ""
 	
 	resp = Response(result)
-	resp.headers['Content-Type'] = content_types[format]
+	resp.headers['Content-Type'] = content_types.get(format)
 
 	return resp
 
@@ -31,10 +42,8 @@ def test_string(format, input_text):
 	try:
 		api_response = requests.get(os.environ["API_URL"] + "/test_string/" + input_text)
 		result = format_result(format, api_response.text)
-		print("after result")
 		return result
 	except:
-		print("expected")
 		pass
 	return ""
 	# return f"{format}, {input_text}"
