245313a82fb848d60bd3f14fcd403dd166829464
Jacek Stasiak <snsvgb@wp.pl>
2021-12-02 13:28:49 +0100

    idk

--- zad5/app.py
+++ zad5/app.py
@@ -1,12 +1,11 @@
 from flask import Flask, Response, request
 import os
 import requests
+from bs4 import BeautifulSoup
 import re
 # from flask import request
 
 app = Flask(__name__)
-
-print(f'Remote api url: {os.environ["API_URL"]}')
 
 content_types = {
 	"txt": "text/plain; charset=UTF-8",
@@ -16,30 +15,6 @@ content_types = {
 	None: "text/plain; charset=UTF-8"
 }
 
-formats = ["txt", "json", "xml", "csv"]
-
-def format_result(format, value):
-	
-	if format == 'txt':
-		result = f"Rodzaj znaków: {value}."
-	elif format == 'json':
-		result = "{ \"rodzaj_znakow\": " + str(value) + " }"
-	elif format == 'xml':
-		result = f"""<?xml version="1.0" encoding="UTF-8"?>
-<rodzaj_znakow>{value}</rodzaj_znakow>
-		"""
-	elif format == 'csv':
-		result = f"""\"Rodzaj znaków\"
-{value}
-		"""
-	else:
-		result = ""
-	
-	resp = Response(result)
-	resp.headers['Content-Type'] = content_types.get(format)
-
-	return resp
-
 regexes = {
 	"json": r"\"rodzaj_znakow\"\:\s\"(\d+)\"",
 	"txt": r"Rodzaj znaków\:\s(\d+)\.",
@@ -47,24 +22,24 @@ regexes = {
 	"csv": r"\"Rodzaj znaków\"\n(\d+)"
 }
 
-@app.route("/convert_string/<string:source_format>/<string:dest_format>/<string:input_text>", methods=["get"])
-def abab(source_format, dest_format, input_text):
-	if source_format in formats and dest_format in formats:
-		try:
-			api_response = requests.get(os.environ["API_URL"] + "/test_string/" + source_format + "/" + input_text)
-			parsed = re.findall(regexes[source_format], api_response.text)[0]
-			result = format_result(dest_format, parsed)
-			return result
-		except Exception as ex:
-			pass
-	return ""
+api_url = "http://panoramafirm.pl/szkoła"
 
-@app.route("/convert_string/<string:source_format>/<string:dest_format>", methods=["post"])
-def aba(source_format, dest_format):
-	if source_format in formats and dest_format in formats:
-		data = request.data.decode("utf-8")
-		parsed = re.findall(regexes[source_format], data)
-		if len(parsed) > 0:
-			result = format_result(dest_format, int(parsed[0]))
-			return result
-	return ""
+@app.route("/query/<string:query_string>", methods=["get"])
+def aba(query_string):
+	result = requests.get(api_url)
+	
+	soup = BeautifulSoup(result.text, 'html.parser')
+	company_list = soup.find(id="company-list")
+
+	ret = "<ul>"
+	for elem in company_list.children:
+		title = elem.find_next(class_="company-name")
+		if title is not None:
+			title = title.get_text()
+		t = f"<li><a href=\"/vcard\">Generuj vcard</a>Nazwa firmy: {title}</li>"
+		ret += t 
+
+	ret += "</ul>"
+
+	return ret
+	
