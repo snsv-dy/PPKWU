49d963208fc2e58e19434799cc6d9e90b2b1a06c
Jacek Stasiak <snsvgb@wp.pl>
2021-12-02 13:50:53 +0100

    a

--- zad5/app.py
+++ zad5/app.py
@@ -22,22 +22,50 @@ regexes = {
 	"csv": r"\"Rodzaj znaków\"\n(\d+)"
 }
 
-api_url = "http://panoramafirm.pl/szkoła"
+api_url = "http://panoramafirm.pl/"
+
+# def genVcard(name, address, phone, email):
+	
+
+@app.route("/vcard/<name>/<address>/<phone>/<email>", methods=["get"])
+def vc(name, address, phone, email):
+	card = f'''BEGIN:VCARD
+VERSION:2.1
+FN:Imie Nazwisko
+N:{name}
+ADR;WORK;PREF;ENCODING=QUOTED-PRINTABLE:;{address}
+TEL;CELL:{phone}
+EMAIL;INTERNET:{email}
+UID:
+END:VCARD'''
+
+	resp = Response(card)
+	resp.headers['Content-Type'] = content_types.get('text/vcard')
+	return resp
 
 @app.route("/query/<string:query_string>", methods=["get"])
 def aba(query_string):
-	result = requests.get(api_url)
+	result = requests.get(api_url + query_string)
 	
 	soup = BeautifulSoup(result.text, 'html.parser')
 	company_list = soup.find(id="company-list")
 
 	ret = "<ul>"
 	for elem in company_list.children:
-		title = elem.find_next(class_="company-name")
-		if title is not None:
-			title = title.get_text()
-		t = f"<li><a href=\"/vcard\">Generuj vcard</a>Nazwa firmy: {title}</li>"
-		ret += t 
+		try:
+			title = elem.find_next(class_="company-name")
+			phone = elem.find_next(class_="icon-telephone").get('title')
+			email = elem.find_next(class_="icon-envelope").get('data-company-email')
+			addr = elem.find_next(class_="address").contents[-1]
+			print("title: ", title)
+			print("phone: ", addr)
+			# print("title: ", title)
+			if title is not None:
+				title = title.get_text()
+			t = f"<li><a href=\"/vcard/{title}/{addr}/{phone}/{email}\">Generuj vcard</a>Nazwa firmy: {title}, telefon: {phone}, email: {email}, address: {addr}</li>"
+			ret += t 
+		except:
+			pass
 
 	ret += "</ul>"
 
